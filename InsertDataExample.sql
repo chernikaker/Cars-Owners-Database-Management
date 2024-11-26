0fea9eb86915d2c4f3a6b92327a37eda82fe9428
Anna Shrubok <annashrubok21@gmail.com>
2024-11-27 00:19:02 +0300

    add insertion data examples

--- InsertDataExample.sql
+++ InsertDataExample.sql
@@ -44,3 +44,20 @@ CREATE TABLE Car (
 	CHECK (sell_date>'1950-01-01' AND sell_date<=GETDATE())
 );
 
+
+INSERT INTO Country (name) VALUES
+('United States'),
+('Germany'),
+('Japan');
+
+
+INSERT INTO Owner (first_name, last_name, birth_date, address, phone_number, email, driving_category, license_date) VALUES
+('John', 'Doe', '1985-05-15', '123 Main St, Springfield', '+1(555)123-4567', 'john.doe@example.com', 'B', '2010-06-01'),
+('Anna', 'Smith', '1990-09-20', '456 Elm St, Metropolis', '+1(555)987-6543', 'anna.smith@example.com', 'B', '2012-03-15'),
+('Michael', 'Johnson', '1975-02-28', '789 Oak St, Gotham', '+1(555)555-5555', 'michael.j@example.com', 'C', '2005-11-20');
+
+-- Вставка записей в таблицу Car
+INSERT INTO Car (car_number, brand, model, production_year, color, price, engine_capacity, sell_date, register_country_id, owner_id, mileage) VALUES
+('ABC123', 'Ford', 'Mustang', 2020, 'Red', 30000.00, 5.0, '2021-07-01', 1, 1, 15000),
+('DEF456', 'BMW', 'X5', 2018, 'Black', 45000.00, 3.0, '2020-01-15', 2, 2, 25000),
+('GHI789', 'Toyota', 'Camry', 2019, 'White', 24000.00, 2.5, '2021-05-20', 3, 3, 18000);
